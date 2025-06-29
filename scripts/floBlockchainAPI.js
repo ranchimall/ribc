ce28176563ab1c18d9e8b9e1340825c6661eeb05
tripathyr <rohit.tripathy@gmail.com>
2025-06-29 08:48:29 +0530

    Update floBlockchainAPI.js

--- scripts/floBlockchainAPI.js
+++ scripts/floBlockchainAPI.js
@@ -6,7 +6,7 @@
     const DEFAULT = {
         blockchain: floGlobals.blockchain,
         apiURL: {
-            FLO: ['https://blockbook.flocard.app/'],
+            FLO:  ['https://blockbook.flocard.app/'],
             FLO_TEST: ['https://blockbook-testnet.ranchimall.net/']
         },
         sendAmt: 0.0003,
